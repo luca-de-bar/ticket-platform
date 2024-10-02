00a4425ecf9345ffc17c587261a718e1089d382d
luca-de-bar <lucadebar33@gmail.com>
2024-10-02 19:36:11 +0200

    add : ER schema

---
+++ src/test/java/com/milestone4/ticketplatform/TicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milestone4.ticketplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
