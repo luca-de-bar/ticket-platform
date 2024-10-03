d6f9863c8fe9ce805a57ea091641be7fa9cf0f1f
luca-de-bar <lucadebar33@gmail.com>
2024-10-03 11:42:11 +0200

    add : js validation for custom login

--- src/main/resources/static/js/loginValidation.js
+++ src/main/resources/static/js/loginValidation.js
@@ -1,21 +1,61 @@
 'use strict'
 
-//ev.preventDefault();
+
 const form = document.getElementById('login-form')
-
-form.addEventListener('submit',function (ev){
-    ev.preventDefault()
-})
-
 //Form Elements
 const usernameInput = document.getElementById('username')
 const passwordInput = document.getElementById('password')
+const userFeeback = document.getElementById('username-invalid-feedback')
+const passwordFeeback = document.getElementById('password-invalid-feedback')
 const submit=document.getElementById('submit')
 
+function validate() {
+    form.addEventListener('submit',function (ev){
+        //If inputs are empty or null
+        if(usernameInput.value.trim() === ""){
+            //prevent submit
+            ev.preventDefault()
 
-submit.addEventListener('click', function(){
-    if(usernameInput.value.trim() === ""){
-        usernameInput.classList.add('!border-red-500')
-    }
-    console.log("Ciaooo")
-})
+            //invalid feedbacks
+            usernameInput.classList.add("border-red-500")
+            userFeeback.innerHTML = "Perfavore, inserisci un username"
+
+
+        }
+        else if (passwordInput.value.trim() === ""){
+            //prevent submit
+            ev.preventDefault()
+
+            //invalid feedbacks
+            passwordInput.classList.add("border-red-500")
+            passwordFeeback.innerHTML = "Perfavore, inserisci una password"
+        }
+    })
+}
+
+function usernameInputCheckState(){
+    usernameInput.addEventListener('change', function () {
+        //username input
+        if(usernameInput.value.trim() !== ""){
+            //Remove invalid feedbacks
+            usernameInput.classList.remove("border-red-500")
+            userFeeback.innerHTML=""
+        }
+    })
+}
+
+function passwordInputCheckState(){
+    passwordInput.addEventListener('change', function () {
+        //username input
+        if(passwordInput.value.trim() !== ""){
+            //Remove invalid feedbacks
+            passwordInput.classList.remove("border-red-500")
+            passwordFeeback.innerHTML=""
+        }
+    })
+}
+
+usernameInputCheckState()
+passwordInputCheckState()
+validate()
+
